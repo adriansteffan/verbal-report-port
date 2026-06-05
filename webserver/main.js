c5131f475d74c146ff051b4da763365f05ed434f
Adrian Steffan <adrian.steffan@hotmail.de>
2026-06-05 10:32:47 +0200

    disable deletion cron job in favor of manual deletion

--- webserver/main.js
+++ webserver/main.js
@@ -28,9 +28,10 @@ winston.add(logger);
 
 
 
-cron.schedule(`${config.TIME_FOR_DELETION_TASK} * * * *`, () => {
-    deleteUnfinishedTrials(config.PATH_TO_RESSOURCES, config.MINUTES_TO_DELETE, logger);
-});
+// Scheduled auto-deletion of unfinished trials disabled for now.
+// cron.schedule(`${config.TIME_FOR_DELETION_TASK} * * * *`, () => {
+//     deleteUnfinishedTrials(config.PATH_TO_RESSOURCES, config.MINUTES_TO_DELETE, logger);
+// });
 
 
 app.listen(config.PORT, () => {
